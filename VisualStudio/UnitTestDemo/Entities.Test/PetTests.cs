5fdb5acc5aac4e7c1e07a2519e9c5069c53ebac5
Stojancho Jefremov <stojanco.jefremov@gmail.com>
2016-06-25 10:26:25 +0200

    VSUnitTestsAdded

---
+++ VisualStudio/UnitTestDemo/Entities.Test/PetTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Entities.Test
+{
+    [TestClass]
+    public class PetTests
+    {
+        [TestMethod]
+        public void Constructor_SetProperties_Valid_Test()
+        {
+            //arrange
+            var name = "Rex";
+            var age = 10;
+
+            //act
+            var rex = new Pet(name, age);
+
+            //assert
+            Assert.AreEqual(name, rex.Name);
+            Assert.AreEqual(age, rex.Age);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]//assert part
+        public void Constructor_SetProperties_Negative_Test()
+        {
+            //arrange
+            var name = "Rex";
+            var age = -10;
+
+            //act
+            var rex = new Pet(name, age);
+                
+        }
+
+
+    }
+}
---
+++ VisualStudio/UnitTestDemo/Entities/Pet.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Pet
+    {
+        public string Name { get; private set; }
+
+        private int age;
+        public int Age
+        {
+            get { return age; }
+            private set
+            {
+                if (value < 0 || value > 20)
+                {
+                    throw new Exception("Invalid value for age!");
+                }
+
+                age = value;
+            }
+        }
+
+        public Pet(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+}
